3ab80a2361b6a8c535e00d7e359899e1426ab7f1
AHN <ga33229977@gmail.com>
2025-09-05 13:50:03 +0900

    04

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/17/40c398c30a8a00101f5b9eab696611ba
@@ -0,0 +1,8 @@
+package com.kh.properties;
+
+public class PropertiesRun {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- PopUp/src/com/kh/pop/controller/popController.java
+++ PopUp/src/com/kh/pop/controller/popController.java
@@ -1,5 +1,13 @@
 package com.kh.pop.controller;
 
+import java.util.List;
+
+import com.kh.pop.model.vo.Popup;
+
 public class PopController {
+	public List<Popup> findAll() {
+		return null;
+		
+	}
 
 }
